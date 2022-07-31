060230ced0e1e4f83afa6fa6a83e4d8b36dc1452
Иван Овсянников <socresist@gmail.com>
2022-07-31 16:29:11 +0300

    квадратное уравнение

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,18 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let rootOne;
+  let rootTwo;
+  let arr=[];
+  let d = b**2-4*a*c;  
+  if (d>0) {
+   rootOne = -b+Math.sqrt(d)/(2*a); 
+   rootTwo = -b-Math.sqrt(d)/(2*a);
+    arr.push (rootOne, rootTwo);
+  } else if (d=0) {
+    rootOne = -b/(2*a); 
+    arr.push (rootOne)}
+  else if (d<0) {arr.push (" ")}; 
+return arr}
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
