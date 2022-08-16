59ffad3d7711ef18916f8ef888df45ab4ade21fd
Иван Овсянников <socresist@gmail.com>
2022-08-16 09:54:53 +0300

    правки в формулу квадратного уравнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -6,13 +6,11 @@ function solveEquation(a, b, c) {
   let arr=[];
   let d = b**2-4*a*c;  
   if (d>0) {
-   rootOne = -b+Math.sqrt(d)/(2*a); 
-   rootTwo = -b-Math.sqrt(d)/(2*a);
+   rootOne = (-b+Math.sqrt(d))/(2*a); 
+   rootTwo = (-b-Math.sqrt(d))/(2*a);
     arr.push (rootOne, rootTwo);
-  } else if (d=0) {
+  } else if (d===0) {
     rootOne = -b/(2*a); 
-    arr.push (rootOne)}
-  else if (d<0) {arr.push (" ")}; 
+    arr.push (rootOne)} 
 return arr}
-
 
