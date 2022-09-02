8848e58af5e5352438f6691ff8a4431ef8c60f3e
Иван Овсянников <socresist@gmail.com>
2022-09-02 09:38:32 +0300

    Задание Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name,
+  this.gender = gender,
+  this.age = age}
+    
+  Student.prototype.setSubject = function (subjectName) {
+      this.subject = subjectName;
+      console.log(subjectName)
+    };
+  
+  Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+      this.marks = new Array();
+      this.marks.push(mark)
+    } 
+    else {
+      this.marks.push(mark)
+           }
+    console.log(this.marks)
+  };
 
-}
+  Student.prototype.addMarks = function(...mark) {
+    if (this.marks === undefined) {
+      this.marks = new Array();
+      this.marks.push(...mark)
+    } else {this.marks.push(...mark)}
+    console.log(this.marks)
+  };
+  
+  Student.prototype.getAverage = function(arr) 
+    {arr = this.marks; 
+     let sum = 0;
+     for (let i = 0; i<this.marks.length; i++) {
+       sum += this.marks[i]; 
+     }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+     console.log(Number(sum/this.marks.length).toFixed(1))
+  
+    }
 
-// ваш код для остальных методов
+  Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason; 
+    console.log(this.excluded)
+  }
